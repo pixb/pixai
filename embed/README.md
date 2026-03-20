9257c3b9274287b5dc7538fc2d6bd4fd8ccc325a
pix <tpxsky@163.com>
2026-03-21 04:06:41 +0800

    chore: modify

---
+++ embed/README.md
@@ -0,0 +1,23 @@
+# embed
+
+`embeding` 模型测试，运行在 `ollama` 上.
+
+```shell
+sudo bash install.sh
+```
+
+```shell
+python -m venv .venv
+```
+
+```shell
+source .venv/bin/activate
+```
+
+```shell
+pip install -r requirements.txt
+```
+
+```shell
+python test_embed.py
+```
--- embed/test_embed.py
+++ embed/test_embed.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 client = OpenAI(base_url="http://localhost:11434/v1", api_key="no-key")
 response = client.embeddings.create(
-    model="mxbai-embed-large",
+    model="mxbai-embed-large:v1",
     input="今天天气真好"
 )
 print(len(response.data[0].embedding))  # 输出: 1024
