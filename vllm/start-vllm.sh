6c39aa6550349760d2cc2d0dbda026551467fc7b
pix <tpxsky@163.com>
2026-08-10 01:16:42 +0800

    feat: add vllm

--- vllm/start-vllm.sh
+++ vllm/start-vllm.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 python -m vllm.entrypoints.openai.api_server \
   --model Qwen/Qwen2.5-3B-Instruct-AWQ \
   --quantization awq_marlin \
   --gpu-memory-utilization 0.8 \
-  --max-model-len 8192 \
+  --max-model-len 16000 \
   --enable-auto-tool-choice \
   --tool-call-parser hermes \
-  --port 8000 \
+  --port 8765 \
   --host 0.0.0.0
